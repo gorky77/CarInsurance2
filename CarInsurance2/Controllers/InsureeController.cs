f9c322e80003bbb0014214fc01b9b72058ef0697
gorkem <gorkemokan@hotmail.com>
2024-09-24 19:59:42 -0400

    First Initialize

---
+++ CarInsurance2/Controllers/InsureeController.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance2.Models;
+
+namespace CarInsurance2.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                // Quote calculation logic
+                insuree.Quote = CalculateQuote(insuree);
+
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                // Quote calculation logic
+                insuree.Quote = CalculateQuote(insuree);
+
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Method for calculating the insurance quote
+        public decimal CalculateQuote(Insuree insuree)
+        {
+            decimal quote = 50m; // Starting base price
+
+            // Adding cost based on the insuree's age
+            var age = DateTime.Now.Year - insuree.DateOfBirth.Year;
+            if (age <= 18)
+            {
+                quote += 100;
+            }
+            else if (age >= 19 && age <= 25)
+            {
+                quote += 50;
+            }
+            else if (age >= 26)
+            {
+                quote += 25;
+            }
+
+            // Adding cost based on the car's year
+            if (insuree.CarYear < 2000)
+            {
+                quote += 25;
+            }
+            else if (insuree.CarYear > 2015)
+            {
+                quote += 25;
+            }
+
+            // Adding cost based on the car's make
+            if (insuree.CarMake == "Porsche")
+            {
+                quote += 25;
+
+                // If the car is a Porsche 911 Carrera, add an additional cost
+                if (insuree.CarModel == "911 Carrera")
+                {
+                    quote += 25;
+                }
+            }
+
+            // Adding cost for each speeding ticket
+            quote += insuree.SpeedingTickets * 10;
+
+            // Adding 25% if the insuree has had a DUI
+            if (insuree.DUI)
+            {
+                quote *= 1.25m;
+            }
+
+            // Adding 50% for full coverage
+            if (insuree.CoverageType)
+            {
+                quote *= 1.50m;
+            }
+
+            return quote;
+        }
+
+        public ActionResult Admin()
+        {
+            var insurees = db.Insurees.ToList();  // Get all the records from the database
+            return View(insurees);  // Send the records to the Admin view
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
